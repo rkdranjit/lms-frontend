6d6deb74dd4bb9ca83c8fadb4baac0102da9fd17
rkdranjit <rkdranjit264@gmail.com>
2023-09-25 18:29:13 +0530

    Added basic layout and integrated to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './components/footer'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <Footer />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+
+import { Link } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+import homePageMainImage from "../assets/Images/homePageMainImage.png"
+
+
+function Home(){
+
+
+    return(
+        <HomeLayout>
+            <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[88vh]">
+                <div className="w-1/2 space-y-6">
+                    <h1 className="text-4xl font-semibold">Find Out Best <span className="text-yellow-500 font-bold">Online Courses</span></h1>
+                    <p className="text-xl text-gray-300">
+                        We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
+                    </p>
+                    <div className="space-x-6">
+                        <Link to="/courses">
+                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-200">
+                                Explore Courses
+                            </button>
+                        </Link>
+                        <Link to="/contacts">
+                            <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-200">
+                                Contact Us
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="w-1/2 flex items-center justify-center">
+                    <img src={homePageMainImage} alt="home page" />
+                </div>
+            </div>
+        </HomeLayout>
+        
+    )
+}
+export default Home;
